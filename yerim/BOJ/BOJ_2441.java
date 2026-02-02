bdb36d642b9b2a61cd6232046eec987943aa7614
leeyerim <ririnye6623@gmail.com>
2026-02-02 17:39:36 +0900

    BOJ_2441_별찍기4

---
+++ yerim/BOJ/BOJ_2441.java
@@ -0,0 +1,27 @@
+//20260202
+//백준 브론즈3 2441 별찍기4
+
+import java.io.*;
+
+public class BOJ_2441 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i; j++) {
+				bw.write(" ");
+			}
+			
+			for (int j = i; j < N; j++) {
+				bw.write("*");
+			}
+			
+			bw.write("\n");
+		}
+		
+		bw.flush();
+	}
+}
