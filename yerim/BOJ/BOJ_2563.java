46b48521ca4dd4015625d2888acb373155e31c4c
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2026-02-27 15:46:27 +0900

    20260204
    Implement solution for BOJ problem 2563 using a 2D array to count colored paper areas.

---
+++ yerim/BOJ/BOJ_2563.java
@@ -0,0 +1,37 @@
+//20260204
+//백준 실버5 2563 색종이
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int tc = Integer.parseInt(br.readLine());
+		int[][] map = new int[100][100];
+		int sum = 0;
+		
+		for (int i = 0; i < tc; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			for (int j = x; j < x + 10; j++) {
+				for (int k = y; k < y + 10; k++) {
+					map[j][k]++;
+				}
+			}
+		}
+		
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map.length; j++) {
+				if (map[i][j] != 0) sum++;
+			}
+		}
+		
+		bw.write(sum + "");
+		bw.flush();
+	}
+}
