aa993be113e3fa0771631ab6167957ee47ac707d
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2026-02-27 16:10:35 +0900

    20260215

--- yerim/BOJ/BOJ_3052.java
+++ yerim/BOJ/BOJ_3052.java
@@ -1,5 +1,8 @@
-//20260213
+//20260215
 //백준 브론즈2 3052 나머지
+
+import java.io.*;
+import java.util.*;
 
 import java.io.*;
 import java.util.*;
@@ -8,19 +11,15 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		Set<Integer> set = new HashSet<>(); //중복 방지하기 위해 HashSet 생성
-		
+
+		Set<Integer> set = new HashSet<>();
+
 		for (int i = 0; i < 10; i++) {
 			int num = Integer.parseInt(br.readLine());
-			
-			if (num < 42)
-				set.add(num);
-			else
-				set.add(num % 42);
+			set.add(num % 42);
 		}
-		
-		bw.write(set.size() + "\n");
+
+		bw.write(set.size() + "");
 		bw.flush();
 	}
 }
