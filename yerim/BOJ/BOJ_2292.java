31698a3d18122cc887ec6c0f54ff69ab83863c57
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2026-02-27 15:49:58 +0900

    20260202

--- yerim/BOJ/BOJ_2292.java
+++ yerim/BOJ/BOJ_2292.java
@@ -3,19 +3,17 @@
 
 import java.io.*;
 
-public class BOJ_2292 {
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int N = Integer.parseInt(br.readLine()); //방의 개수
+		int n = Integer.parseInt(br.readLine());
+		int count = 1;
+		int roomNum = 1;
 		
-		int roomNum = 1; //지나갈 수 있는 방의 범위
-		int count = 1; //지나가는 방의 개수(정답) -> 1번 방부터 시작하므로 초기값은 1
-		
-		//지나갈 수 있는 방의 범위가 방의 개수보다 작거나 같을 때까지만 반복문 실행
-		while (roomNum < N) {
-			roomNum += count * 6; //육각형 모양이므로 6씩 곱하기
+		while (roomNum < n) {
+			roomNum += count * 6;
 			count++;
 		}
 		
