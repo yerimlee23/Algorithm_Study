ea436cb3e1fb7f922cfc041e2a6bde4dce370b38
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2026-02-27 16:49:52 +0900

    BOJ_15652_N과M(4)

---
+++ yerim/BOJ/BOJ_15652.java
@@ -0,0 +1,48 @@
+//20260227
+//백준 실버3 15652 N과M(4)
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n;
+	static int m;
+	static int[] arr;
+	static int[] result;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		result = new int[m];
+		int num = 1;
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = num++;
+		}
+		
+		backtracking(0, 1);
+		bw.write(sb.toString());
+		bw.flush();
+	}
+	
+	public static void backtracking(int idx, int start) {
+		if (idx == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(result[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = start; i <= n; i++) {	
+			result[idx] = i;
+			backtracking(idx + 1, i);
+		}
+	}
+}
