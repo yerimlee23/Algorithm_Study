752f51f84ac11c7b19ae76f6cf719f700e66e660
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2026-02-27 16:37:14 +0900

    BOJ_2669_직사각형네개의합집합의면적구하기

---
+++ yerim/BOJ/BOJ_2669.java
@@ -0,0 +1,40 @@
+//20260218
+//백준 실버5 2669 직사각형네개의합집합의면적구하기
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int[][] map = new int[100][100];
+		int count = 0;
+		
+		for (int i = 0; i < 4; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			int q = Integer.parseInt(st.nextToken());
+			
+			for (int j = x; j < p; j++) {
+				for (int k = y; k < q; k++) {
+					map[j][k] = 1;
+				}
+			}
+		}
+		
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map.length; j++) {
+				if (map[i][j] == 1) {
+					count++;
+				}
+			}
+		}
+		
+		bw.write(count + "");
+		bw.flush();
+	}
+}
